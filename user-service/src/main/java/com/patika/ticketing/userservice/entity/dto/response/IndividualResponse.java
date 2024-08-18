bb0f9d5f2e0ae4134a102fbe1a33168aeda3b370
BeyzaNurEkiz <beyzanurr8@outlook.com>
2024-08-18 22:17:51 +0300

    response güncellendi

--- user-service/src/main/java/com/patika/ticketing/userservice/entity/dto/response/IndividualResponse.java
+++ user-service/src/main/java/com/patika/ticketing/userservice/entity/dto/response/IndividualResponse.java
@@ -2,11 +2,21 @@ package com.patika.ticketing.userservice.entity.dto.response;
 
 import lombok.*;
 
+import java.util.Set;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class IndividualResponse extends BaseResponse {
+public class IndividualResponse {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String phoneNumber;
+    private Set<String> roles;
     private String tcNumber;
 
 }
