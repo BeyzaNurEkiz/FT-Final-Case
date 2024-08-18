832624ad2cd0a348625372943bd34199d165d744
BeyzaNurEkiz <beyzanurr8@outlook.com>
2024-08-18 14:10:01 +0300

    readme eklendi

--- online-ticketing-system/src/main/java/com/patika/ticketing/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.patika.ticketing.repository;
-
-import com.patika.ticketing.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
-}
