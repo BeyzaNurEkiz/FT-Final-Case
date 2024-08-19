81b60062c6b12bc156ef05b0738cca60fa2df4e8
BeyzaNurEkiz <beyzanurr8@outlook.com>
2024-08-19 21:38:28 +0300

    role repository eklendi

---
+++ user-service/src/main/java/com/patika/ticketing/userservice/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.patika.ticketing.userservice.repository;
+
+import com.patika.ticketing.userservice.entity.ERole;
+import com.patika.ticketing.userservice.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    @Query("SELECT role FROM Role role WHERE role.name = :name")
+    Optional<Role> findByName(@Param("name") ERole name);
+}
