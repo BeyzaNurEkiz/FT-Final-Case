2202af46809948b7856caa02274c508489fb1de7
BeyzaNurEkiz <beyzanurr8@outlook.com>
2024-08-23 22:59:38 +0300

    IndividualUserRepository eklendi

--- user-service/src/main/java/com/patika/ticketing/userservice/repository/IndividualUserRepository.java
+++ user-service/src/main/java/com/patika/ticketing/userservice/repository/IndividualUserRepository.java
@@ -4,7 +4,14 @@ import com.patika.ticketing.userservice.entity.IndividualUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IndividualUserRepository extends JpaRepository<IndividualUser, Long> {
+    Optional<IndividualUser> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
 
